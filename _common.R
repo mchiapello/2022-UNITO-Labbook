f46688ed8e7013934e34354945de67b68130c724
Marco Chiapello <chiapello.m@gmail.com>
2022-01-18 10:21:13 +0100

    update

---
+++ _common.R
@@ -0,0 +1,14 @@
+knitr::opts_chunk$set(
+  comment = "#>",
+  echo = FALSE,
+  collapse = TRUE,
+  cache = TRUE,
+  warning = FALSE,
+  message = FALSE,
+  dpi = 300,
+  cache.lazy = FALSE,
+  out.width = "90%",
+  fig.align = "center",
+  fig.width = 5,
+  fig.height = 7
+)
