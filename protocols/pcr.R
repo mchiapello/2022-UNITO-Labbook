d698ae747f5d15761ef2d9ee0477730ab9113904
Marco Chiapello <chiapello.m@gmail.com>
2022-01-19 09:26:25 +0100

    WIP: pcr table

--- protocols/pcr.R
+++ protocols/pcr.R
@@ -1,6 +1,8 @@
+reac <- 10
 pcr <- tibble(
               Reagents = c("Buffer", "dNTPs", "Forward Primer", "Reverse Primer",
                            "H2O", "Template"),
               InitialConcetration = NA,
-              Volume = c(10, 2, 1, 1, 35, 1)
+              VolumeInit = c(10, 2, 1, 1, 35, 1),
+              VolumeFinal = VolumeInit * reac
 )
